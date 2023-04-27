9ae9178a2fd1da6c04d2d25db219386660824c8c
Sampad Kumar Kar <sampaddavcspur2001@gmail.com>
2023-04-28 02:48:38 +0530

    Added Assignment 4, Updated a few files

--- assign4_sms_spam_detection_app_dockerized/test/integration_test.py
+++ assign4_sms_spam_detection_app_dockerized/test/integration_test.py
@@ -20,57 +20,13 @@ app_path = config['app_path']
 # extract sample sms text (spam)
 input_text = config['input_text']
 
-'''
 def test_app():
     # start the flask app using command_line
     cmd = 'python ' + app_path + ' &'
     os.system(cmd)
 
     # wait for the app to start up
-    time.sleep(1)
-
-    # wait for the app to start listening
-    endpoint = "http://localhost:5000/score"
-    timeout = 5
-    while True:
-        try:
-            response_get = requests.get(endpoint, timeout=timeout)
-            if response_get.ok:
-                break
-        except:
-            pass
-    
-    # assert whether the response is what we expect
-    assert (response_get.status_code == 200)
-    
-    # send a POST request with input message
-    sms_text = input_text
-    # extract response
-    response_post = requests.post(endpoint, data={"sms_text":sms_text})
-
-    # check the response
-    assert response_post.ok
-    
-    # extract model output
-    output_dict = response_post.json()
-    assert "prediction" in output_dict
-    assert "propensity" in output_dict
-    
-    # assert that the model is detecting the message as True (spam)
-    assert output_dict["prediction"] == True
-
-
-    # stop the Flask app using command line
-    os.system('kill $(lsof -t -i:5000)')
-'''
-
-def test_app():
-    # start the flask app using command_line
-    cmd = 'python ' + app_path + ' &'
-    os.system(cmd)
-
-    # wait for the app to start up
-    time.sleep(1)
+    time.sleep(5)
 
     # wait for the app to start listening
 
